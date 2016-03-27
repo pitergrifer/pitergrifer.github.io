1ceb8cc8f7863056f143f704e8e34e9ad55aedb5
Bogdan Danileichenko <danileichenko.bogdan@mail.ru>
2016-03-27 18:02:21 +0300

    ok

--- js/scripts.js
+++ js/scripts.js
@@ -1,225 +1,75 @@
 $(document).ready(function () {
-  /* Кнопка "Бургер" */
-  $('.social-links-drop-menu').click(function () {
-    $(this).toggleClass('social-links-drop-menu-active');
-    $('.social-links-mobile').toggleClass('social-links-mobile-active');
+  /* Скрипт, задающий вариативную цветовую палитру превью постов */
+  var postArray = $('.post');
+  var maskArray = $('.mask');
+  var borderColors = ['#14b9d6', '#e76049', '#2eb398', '#90899c', '#5692b1', '#f9b704', '#dd1787', '#e76049', '#585858'];
+  var maskColors = ['rgba(20, 185, 214, .8)', 'rgba(231, 96, 73, .8)', 'rgba(46, 179, 152, .8)', 'rgba(144, 137, 156, .8)', 'rgba(86, 146, 177, .8)', 'rgba(249, 183, 4, .8)', 'rgba(221, 23, 135, .8)', 'rgba(231, 96, 73, .8)', 'rgba(88, 88, 88, .8)'];
+
+  for (var i = 0; i < postArray.length; i++) {
+    postArray.eq(i).css({
+      'border-bottom': '5px solid',
+      'border-bottom-color': borderColors[i]
+    });
+    maskArray.eq(i).css({
+      'background-color': maskColors[i]
+    });
+  };
+
+  /* Скрипт, задающий высоту меню категорий */
+  var knowHeight = parseInt($('.height').height());
+  $('.categories').css('height', knowHeight);
+
+  /* Скрипт, расчитывающий параметры внутрипостовых блоков картинок */
+  $(window).load(function () {
+    var overlayImgArray = $('.inpost-img p');
+    for (var i = 0; i < overlayImgArray.length; i++) {
+      overlayImgArray.eq(i).css({
+        'background-color': maskColors[i],
+        'margin-top': overlayImgArray.eq(i).height() * -1 - 32
+      });
+    };
   });
 
-  /* Мобильное меню */
-  $('.mobile-menu-button').click(function () {
-    $(this).toggleClass('mobile-menu-button-active');
-    $('.life-style-navbar-mobile').toggleClass('life-style-navbar-mobile-active');
+  /* Скрипт просмотра увеличенного изображения */
+  var imgWrapper = $('.img-wrapper');
+  var inpostImg = $('.inpost-img img');
+  var imgWindow = $('.big-img-window');
+  var inWrapperImg = $('.img-wrapper img');
+  var windowHeight = $(window).height();
+  var loader = $('.loader');
+
+  inWrapperImg.css({
+    'max-height': windowHeight * 0.9 - 100
   });
 
-  /* Скрипт активации поля поиска */
-  $('.ls-button.search').click(function () {
-    $(this).toggleClass('search-active');
-    $('.submit-story').toggleClass('submit-story-deactive');
-    $('.search-field').toggleClass('search-field-active');
-  });
-
-  /* Скрипт работы карусели */
-  function rotation() {
-    setTimeout(function () {
-      if ($('.ls-arrow.right').attr('avalible') == 'false') {
-        $('.ls-arrow.right').click(function () {
-          //do nothing
+  inpostImg.click(function () {
+    imgWindow.addClass('big-img-window-active');
+    loader.addClass('loader-on');
+    if (imgWindow.hasClass('big-img-window-active')) {
+      inWrapperImg.load(function () {
+        loader.removeClass('loader-on');
+        setTimeout(function () {
+          imgWrapper.addClass('img-wrapper-drop');
+        }, 1);
+        var wrapperWidth = imgWrapper.width() + 60;
+        var wrapperHeight = imgWrapper.height() + 60;
+        imgWrapper.css({
+          'marginLeft': wrapperWidth * -0.5,
+          'marginTop': wrapperHeight * -0.5
         });
-      } else if ($('.image:last').hasClass('active')) {
-        $('.image.active').addClass('left');
-        $('.image:first').addClass('next');
-        setTimeout(function () {
-          $('.image:first').addClass('current');
-          activateCT();
-        }, 1);
-        setTimeout(function () {
-          $('.image.active').removeClass('active').removeClass('left');
-          $('.image:first').removeClass('next').removeClass('current').addClass('active');
-          $('.ls-arrow.right').removeAttr('avalible');
-        }, 500);
-      } else {
-        $('.image.active').addClass('left').next('.image').addClass('next');
-        setTimeout(function () {
-          $('.image.active').next('.image').addClass('current');
-          activateCT();
-        }, 1);
-        setTimeout(function () {
-          $('.image.active').removeClass('active').removeClass('left').next('.image').removeClass('next').removeClass('current').addClass('active');
-          $('.ls-arrow.right').removeAttr('avalible');
-        }, 500);
-      };
-      $('.ls-arrow.right').attr('avalible', 'false');
-    }, 3000);
-  };
-
-  function rotationManager(flag, rotation, time) {
-    if (flag)
-      intervalID = setInterval(rotation, time);
-    else
-      clearInterval(intervalID);
-  }
-
-  rotationManager(true, rotation, 2000);
-
-  $('#ls-carousel').hover(function () {
-    rotationManager(false);
-  }, function () {
-    rotationManager(true, rotation, 2000);
-  });
-
-  function activateCT() {
-    $('.life-style-carousel-indi li').removeClass('active');
-    $('.image').each(function () {
-      var activeImage = $('.image').index($('.image.current'));
-      var toNumber = parseInt(activeImage);
-      var titles = ['#carousel-title-1', '#carousel-title-2', '#carousel-title-3'];
-      $(titles[toNumber]).addClass('active');
-    });
-  };
-
-  $('.ls-arrow.right').click(function () {
-    if ($('.ls-arrow.right').attr('avalible') == 'false') {
-      $('.ls-arrow.right').click(function () {
-        //do nothing
-      });
-    } else if ($('.image:last').hasClass('active')) {
-      $('.image.active').addClass('left');
-      $('.image:first').addClass('next');
-      setTimeout(function () {
-        $('.image:first').addClass('current');
-        activateCT();
-      }, 1);
-      setTimeout(function () {
-        $('.image.active').removeClass('active').removeClass('left');
-        $('.image:first').removeClass('next').removeClass('current').addClass('active');
-        $('.ls-arrow.right').removeAttr('avalible');
-      }, 500);
-    } else {
-      $('.image.active').addClass('left').next('.image').addClass('next');
-      setTimeout(function () {
-        $('.image.active').next('.image').addClass('current');
-        activateCT();
-      }, 1);
-      setTimeout(function () {
-        $('.image.active').removeClass('active').removeClass('left').next('.image').removeClass('next').removeClass('current').addClass('active');
-        $('.ls-arrow.right').removeAttr('avalible');
-      }, 500);
-    };
-    $('.ls-arrow.right').attr('avalible', 'false');
-  });
-
-  $('.ls-arrow.left').click(function () {
-    if ($('.ls-arrow.left').attr('avalible') == 'false') {
-      $('.ls-arrow.left').click(function () {
-        //do nothing
-      });
-    } else if ($('.image:first').hasClass('active')) {
-      $('.image.active').addClass('right');
-      $('.image:last').addClass('prev');
-      setTimeout(function () {
-        $('.image:last').addClass('current');
-        activateCT();
-      }, 1);
-      setTimeout(function () {
-        $('.image.active').removeClass('active').removeClass('right');
-        $('.image:last').removeClass('prev').removeClass('current').addClass('active');
-        $('.ls-arrow.left').removeAttr('avalible');
-      }, 500);
-    } else {
-      $('.image.active').addClass('right').prev('.image').addClass('prev');
-      setTimeout(function () {
-        $('.image.active').prev('.image').addClass('current');
-        activateCT();
-      }, 1);
-      setTimeout(function () {
-        $('.image.active').removeClass('active').removeClass('right').prev('.image').removeClass('prev').removeClass('current').addClass('active');
-        $('.ls-arrow.left').removeAttr('avalible');
-      }, 500);
-    };
-    $('.ls-arrow.left').attr('avalible', 'false');
-  });
-
-  $('.life-style-carousel-indi li').each(function () {
-    $('.image').each(function () {
-      $('.life-style-carousel-indi li').click(function () {
-        var activatedTitle = parseInt($('.life-style-carousel-indi li').index(this));
-        var activeImage = parseInt($('.image').index($('.image.active')));
-        var imageArray = $('.image');
-        if (activatedTitle > activeImage) {
-          $('.image.active').addClass('left');
-          imageArray.eq(activatedTitle).addClass('next');
-          setTimeout(function () {
-            imageArray.eq(activatedTitle).addClass('current');
-            activateCT();
-          }, 1);
-          setTimeout(function () {
-            $('.image.active').removeClass('active').removeClass('left');
-            imageArray.eq(activatedTitle).removeClass('next').removeClass('current').addClass('active');
-            $('.ls-arrow.right').removeAttr('avalible');
-            $('.ls-arrow.left').removeAttr('avalible');
-          }, 500);
-          $('.ls-arrow.right').attr('avalible', 'false');
-          $('.ls-arrow.left').attr('avalible', 'false');
-        } else if (activatedTitle < activeImage) {
-          $('.image.active').addClass('right');
-          imageArray.eq(activatedTitle).addClass('prev');
-          setTimeout(function () {
-            imageArray.eq(activatedTitle).addClass('current');
-            activateCT();
-          }, 1);
-          setTimeout(function () {
-            $('.image.active').removeClass('active').removeClass('right');
-            imageArray.eq(activatedTitle).removeClass('prev').removeClass('current').addClass('active');
-            $('.ls-arrow.right').removeAttr('avalible');
-            $('.ls-arrow.left').removeAttr('avalible');
-          }, 500);
-          $('.ls-arrow.right').attr('avalible', 'false');
-          $('.ls-arrow.left').attr('avalible', 'false');
-        };
-      });
-    });
-  });
-
-  /* Скрипт работы инстаграмной карусели */
-  var imgInstaArray = $('.insta-slider ul li');
-  var checkImgWidth = parseInt($('.insta-slider ul li').width());
-  var width = 200;
-  var count = imgInstaArray.length;
-  var currentPosition = 0;
-  var sliderSelect = $('.insta-slider ul');
-  var limitRotation = Math.min(((count * width) - width) * -1);
-  $('.instagram-arrow.left').click(function () {
-    if (currentPosition == 0) {
-      $('.instagram-arrow.left').addClass('insta-arrow-disabled');
-    } else {
-      currentPosition = Math.min(currentPosition + width);
-      $('.instagram-arrow.right').removeClass('insta-arrow-disabled');
-      $('.instagram-arrow.left').removeClass('insta-arrow-disabled');
-      $('#rotator').css('margin-left', currentPosition);
-    };
-  });
-  $('.instagram-arrow.right').click(function () {
-    if (currentPosition == limitRotation) {
-      $('.instagram-arrow.right').addClass('insta-arrow-disabled');
-    } else {
-      currentPosition = Math.max(currentPosition - width);
-      $('.instagram-arrow.right').removeClass('insta-arrow-disabled');
-      $('.instagram-arrow.left').removeClass('insta-arrow-disabled');
-      $('#rotator').css('margin-left', currentPosition);
+        $('.img-wrapper a').attr('href', $(this).attr('src'));
+      }).attr('src', $(this).attr('src'));
     };
   });
 
-  /* Скрипт модального окна */
-  $('.can-be-big').click(function () {
-    $(this).clone(false, false).prependTo('.container-big-img');
-    $('.modal-img').toggleClass('slide-down-modal');
-    $('.container-big-img .can-be-big').removeClass('can-be-big');
-  });
-  $('.toggle-modal').click(function () {
-    $('.modal-img').toggleClass('slide-down-modal');
+  $('.big-img-close').click(function () {
+    imgWrapper.addClass('jump-big-img');
     setTimeout(function () {
-      $('.container-big-img img').remove();
-    }, 300);
+      imgWindow.removeClass('big-img-window-active');
+      imgWrapper.removeClass('img-wrapper-drop');
+    }, 600);
+    setTimeout(function () {
+      imgWrapper.removeClass('jump-big-img');
+    }, 700);
   });
-
-}); // Ready end
+}); //ready end
