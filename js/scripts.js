516201e8b0e77153b63f02fbb54c1a7769e864ad
Bogdan Danileichenko <danileichenko.bogdan@mail.ru>
2016-06-18 21:11:08 +0300

    ok

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ function AskingDate(options) {
   var result = options.result;
     
   // вертикальная отцентровка объекта относительно окна
-  var newTop = (document.body.clientHeight / 2) - (elem.offsetHeight / 2);
+  var newTop = (document.documentElement.clientHeight / 2) - (elem.offsetHeight / 2);
   elem.style.top = newTop + "px";
   
   // функция проверки валидности ввода 
@@ -110,14 +110,16 @@ function AskingDate(options) {
     
     if (target.getAttribute('type') != 'text') return;
     
-    // создание элемента-подсказки и его позиционирование
+    // создание элемента-подсказки
     var helper = document.createElement('span');
     helper.className = 'helper';
     var helperText = target.getAttribute('data-helper');
     helper.innerHTML = helperText;
-    var targetCoords = target.getBoundingClientRect();
     document.body.appendChild(helper);
-    helper.style.top = targetCoords.top - helper.offsetHeight - 10 + "px";
+    
+    // позиционирование элемента-подсказки
+    var targetCoords = target.getBoundingClientRect();
+    helper.style.top = targetCoords.top - helper.offsetHeight - 10 + document.documentElement.scrollTop + "px";
     helper.style.left = targetCoords.left - (helper.offsetWidth / 2) + (target.offsetWidth / 2) + "px";
     
     //создание декоративной срелки
@@ -125,7 +127,7 @@ function AskingDate(options) {
     helperArrow.className = 'arrow';
     helper.appendChild(helperArrow);
     helperArrow.style.top = helper.offsetHeight + "px";
-    helperArrow.style.left = (helper.offsetWidth / 2) - (helperArrow.offsetWidth / 2) + "px"; 
+    helperArrow.style.left = (helper.offsetWidth / 2) - (helperArrow.offsetWidth / 2) + "px";   
     
     // удаление подсказки после потери фокуса на цели
     target.onblur = function() {
