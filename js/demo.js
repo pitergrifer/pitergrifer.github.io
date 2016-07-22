3f3c863179d69c6a64a00ed4045b8050c7ec4992
Bogdan Danileichenko <danileichenko.bogdan@mail.ru>
2016-07-22 18:50:53 +0300

    ok

--- js/demo.js
+++ js/demo.js
@@ -15,14 +15,18 @@ container.scrollable({
   sliderHeight: "auto", // высота ползунка ("auto" - расчитывается в зависимости от контента; число (без указания пикселей или процентов) - высота в пикселях)
   sliderHeightMin: 30, // минимальная высота ползунка в пикселях (указывать еденицу измерения ненужно)
   sliderShift: true, // наличие смещения контента для скроллера (флаги "true", "false")
-  horizontalScrolling: "auto", // наличие горизонтальной прокрутки (флаги "true", "false" и "auto")
+  horizontalScrolling: true, // наличие горизонтальной прокрутки (флаги "true", "false" и "auto")
   scrollerXClass: "scroller-x-field",
   stepMultipler: 15, // скорость прокрутки
   scrollBySelection: true, // возможность прокрутки при выделении текста (флаги "true", "false")
   useWheelScroll: true, // возможность прокрутки колесиком мыши (флаги "true", "false")
   useKeyboardScroll: true, // возможность прокрутки клавишами "Стрелки", "PageUp" и "PageDown" (флаги "true", "false")
+  dynamicContent: true,
+  contentResize: false,
   autoHide: true, // наличие эффекта исчезающей полосы прокрутки (флаги "true", "false")
   scrollerOpacityActive: 1, // прозрачность в активном состоянии
   scrollerOpacityPassive: 0.5, // прозрачность в пассивном состоянии
   scrollerOpacityHidden: 0.2, // прозрачность в "спрятанном" состоянии
+  smoothlyScroll: true,
+  smoothlyScrollOptions: "0.3s all"
 });
